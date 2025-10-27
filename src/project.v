9f467d72e74380dbc6e2a408d7b0520e652f2af1
Juan Manuel Tapias Pinzón <111985117+jtapiaspunal@users.noreply.github.com>
2025-10-26 21:27:30 -0500

    Add files via upload

--- src/project.v
+++ src/project.v
@@ -5,7 +5,7 @@
 
 `default_nettype none
 
-module tt_um_example (
+module tt_um_femto (
     input  wire [7:0] ui_in,    // Dedicated inputs
     output wire [7:0] uo_out,   // Dedicated outputs
     input  wire [7:0] uio_in,   // IOs: Input path
@@ -17,11 +17,34 @@ module tt_um_example (
 );
 
   // All output pins must be assigned. If not used, assign to 0.
-  assign uo_out  = ui_in + uio_in;  // Example: ou_out is the sum of ui_in and uio_in
+  
   assign uio_out = 0;
   assign uio_oe  = 0;
 
+  /* verilator lint_off SYNCASYNCNET */
+  reg resetn;
+  /* verilator lint_on SYNCASYNCNET */
+  always @(negedge clk) resetn <= rst_n;
+
+  femto femto0(
+    .clk(clk),
+    .resetn(resetn),
+    .spi_mosi(uo_out[0]),
+    .spi_mosi_ram(uo_out[1]),
+    .spi_cs_n(uo_out[2]),
+    .spi_cs_n_ram(uo_out[3]),
+    .spi_clk_ram(uo_out[4]),
+    .spi_clk(uo_out[5]),
+    .LEDS(uo_out[6]),
+    .TXD(uo_out[7]),
+
+    .spi_miso(ui_in[0]),
+    .spi_miso_ram(ui_in[1]),
+    .RXD(ui_in[2])
+  );
+
+
   // List all unused inputs to prevent warnings
-  wire _unused = &{ena, clk, rst_n, 1'b0};
+  wire _unused = &{ena,uio_in[7:0],ui_in[7:3], 1'b0};
 
 endmodule
